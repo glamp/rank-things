a29e409eccdbdd763602709570ea63fd8d94f9a4
Greg Lamp <lamp.greg@gmail.com>
2018-08-19 12:17:54 -0600

    db

--- server/db.js
+++ server/db.js
@@ -1,12 +1,23 @@
 const knex = require('knex');
 const fs = require('fs');
 const path = require('path');
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, './mydb.sqlite'),
-    useNullAsDefault: true
-  }
-});
+
+let options = {};
+if (process.env.DATABASE_URL) {
+  options = {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  };
+} else {
+  options = {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, './mydb.sqlite'),
+      useNullAsDefault: true
+    }
+  };
+}
+
+const db = knex(options);
 
 module.exports = db; 
